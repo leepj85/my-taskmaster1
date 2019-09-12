dda72405d461c0355a5608c9982cf9b98d315787
Peter Lee <leepj85@gmail.com>
2019-09-12 11:54:38 -0700

    taskmaster working with dynmaodb

--- src/main/java/com/poudel/taskmaster/repository/TaskRepository.java
+++ src/main/java/com/poudel/taskmaster/repository/TaskRepository.java
@@ -4,10 +4,11 @@ import com.poudel.taskmaster.model.Task;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @EnableScan
 public interface TaskRepository extends CrudRepository<Task, String> {
     Optional<Task> findById(String id);
-    Optional<Task> findByAssignee(String assignee);
+    List<Task> findAllByAssignee(String assignee);
 }
